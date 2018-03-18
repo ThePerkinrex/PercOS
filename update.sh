f055e6341c67ce985f807a45689ca50ae191b596
ThePerkinrex <theperkinrex@gmail.com>
2018-03-18 12:23:15 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
+echo "Downloading PercScript"
 curl https://codeload.github.com/ThePerkinrex/PercScript/zip/master --output tmp.zip
+echo "Installing Percscript"
 unzip -u tmp.zip
 rm tmp.zip
 mv PercScript-master/ PercScript/
+echo "PercScript done"
