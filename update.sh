9278d881ea173796fed038a79458d04807154439
theperkinrex <theperkinrex@gmail.com>
2018-03-18 13:06:21 +0100

    Get update packages

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 echo "Downloading PercScript"
 curl https://codeload.github.com/ThePerkinrex/PercScript/zip/master --output tmp.zip
 echo "Installing Percscript"
-unzip -u tmp.zip -x '*.md' -x '*.png' -x 'LICENSE' -x '.gitignore' -x 'Example.plang'
+unzip -u tmp.zip -x '*.md' -x '*.png' 'LICENSE' '.gitignore' 'Example.plang'
 rm tmp.zip
 cp -R PercScript-master/* PercScript/
 rm -r PercScript-master
